e7126a1ebbf949ceffa87f984a6a2487d86d3ce9
David Moates <david.m.moates@gmail.com>
2019-11-08 20:37:06 -0600

    Finished the header. Now moving on to the Tabs.

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,46 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+const Header = () => {
+
+  //Creating functions to uses to make elements.
+  const divCr = () => {
+    const div = document.createElement('div');
+    div.classList.add('header');
+    return div
+  };
+
+  const spanCr = (newClass, content) => {
+    const span = document.createElement('span');
+    span.classList.add(newClass);
+    span.textContent = content;
+    return span
+  };
+
+    const h1Cr = () => {
+      const h1 = document.createElement('h1');
+      h1.textContent = 'Lambda Times';
+      return h1
+    };
+
+    // Create variable to help with speed of appending and make code more functional
+
+    const
+      header = divCr(),
+      date = spanCr('date', 'SMARCH 28, 2019'),
+      title = h1Cr(),
+      temp = spanCr('temp', '98°');
+
+    // Select the parent to append to with querySelector
+
+    const parent = document.querySelector('.header-container');
+
+    // Append the children to the parent
+
+    parent.appendChild(header);
+    header.appendChild(date);
+    header.appendChild(title);
+    header.appendChild(temp);
+};
+
+Header();
